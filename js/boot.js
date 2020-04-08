83ca8bbf3666211457053defb1254d45a3f53df5
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2020-04-07 23:52:50 -0700

    update

--- js/boot.js
+++ js/boot.js
@@ -4,39 +4,24 @@ const FIELD_WIDTH = 15;
 const FIELD_HEIGHT = 20;
 const MAX_BLOCKS = 164;
 const ROW_FULL = 11;
-const BLOCK_SIZE = 200;
 
-var intro_state = false;
-var gameover = false;
-var start_level, turn; //start from level selected under options
+var varro_state = 0;
+var gameover = 0;
+var start_level, turn = 0; //start from level selected under options
 var start_blocks; //number of initial rows on the map
-var block = [4, 4]; //current block
-var newblock = [4, 4];
+var block = [4][4]; //current block
+var newblock = [4][4];
 var next_block, next_next_block; //next block to be put
-var map = [FIELD_WIDTH, FIELD_HEIGHT]; //map
+var map = [FIELD_WIDTH][FIELD_HEIGHT]; //map
 var block_x, block_y; //location of the block
 var x_size, y_size; //size of the block
 var next_x_size, next_y_size; //size of the block
+var timer; //timer of the game
 var current_newblock, current_block; //current block type
 var block_color, newblock_color, oldblock_color;
 var speed; //speed of the game
 var score, dels; //game score and number of rows deleted
 var high_score;
 var Level;
-var numblocks, Index, blockcount;
-var PushDown, Rotate = false;
-var Sprite = {
-  state, //the state of the object (general)
-  anim_state, //an animation state variable, up to you
-  attr, //attributes pertaining to the object (general)
-  x, //position bitmap will be displayed at
-  y,
-  xv, //velocity of object
-  yv,
-  width,
-  height, //the width and height of the bob
-  width_fill, //internal, used to force 8*x wide surfaces
-  counter_1, //general counters
-  counter_2
-};
-//var blocks[MAX_BLOCKS], newblocks[5], oldblocks[MAX_BLOCKS];
+var numblocks, index, blockcount;
+var startGame = false;
---
+++ js/preload.js
@@ -0,0 +1,63 @@
+function preload() {
+
+  this.scale.pageAlignHorizontally = true;
+  this.scale.pageAlignVertically = true;
+  this.scale.refresh();
+
+  showLoader(this);
+  this.load.path = '../assets/';
+  this.load.image('splash', 'splash.bmp');
+  this.load.image('maxxdaddy', 'maxxdaddy.gif');
+  this.load.spritesheet('blocks', 'blocks.png', {
+    frameWidth: 20,
+    frameHeight: 20
+  }, );
+
+}
+
+function showLoader(game) {
+  var progressBar = game.add.graphics();
+  var progressBox = game.add.graphics();
+  progressBox.fillStyle(0x222222, 0.8);
+  progressBox.fillRect(340, 270, 320, 50);
+  game.load.on('progress', function (value) {});
+
+  game.load.on('fileprogress', function (file) {});
+
+  game.load.on('complete', function () {
+    progressBar.destroy();
+    progressBox.destroy();
+    loadingText.destroy();
+    percentText.destroy();
+  });
+  game.load.on('progress', function (value) {
+    progressBar.clear();
+    progressBar.fillStyle(0xff4500, 1);
+    progressBar.fillRect(350, 280, 300 * value, 30);
+    percentText.setText(parseInt(value * 100) + '%');
+  });
+
+  var width = game.cameras.main.width;
+  var height = game.cameras.main.height;
+  var loadingText = game.make.text({
+    x: width / 2,
+    y: height / 2 - 50,
+    text: 'Loading...',
+    style: {
+      font: '20px monospace',
+      fill: '#ffffff'
+    }
+  });
+  loadingText.setOrigin(0.5, 0.5);
+
+  var percentText = game.make.text({
+    x: width / 2,
+    y: height / 2 - 5,
+    text: '0%',
+    style: {
+      font: '18px monospace',
+      fill: '#ffffff'
+    }
+  });
+  percentText.setOrigin(0.5, 0.5);
+}
